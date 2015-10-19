05dfd2b4459bc32bcf96e17d441db2f76597bfff
Anton Malkov <anton.malkov@gmail.com>
2015-10-20 00:59:17 +0200

    Automated commit by anton-bash-script on 2015-10-20_00-59-17

--- multiline.rb
+++ multiline.rb
@@ -1,39 +1,64 @@
+require 'pry'
+
+
 class String
   def undent
-    indent = self.scan(/^[ \t]*(?=\S)/).min.size || 0
-    self.gsub(/^[ \t]{#{indent}}/, '')
+    # indent = self.scan(/^[ \t]+(?=\S)/).min
+    # if indent
+    #   self.split(/^[ \t]{#{indent}}/).map(&:rstrip).join
+    # else
+    #   self
+    # end
+    self.split(/^[ \t]+/).map(&:rstrip).join
   end
 end
 
 
-puts
-puts "     f;dlf;f
-  ;dl;k
-    ;lgk;lk".undent
+# str = "Lorem ipsum dolor sit amet, consectetur          adipisicing
+#       elit, sed do eiusmod tempor incididunt ut labore et dolore
+#       magna aliqua. \nUt enim ad minim veniam, quis nostrud
+#       exercitation ullamco laboris nisi ut aliquip ex ea commodo
+#       consequat. Duis aute irure dolor in reprehenderit in
+#       voluptate.".undent
+#
+# puts str
+
+#
+str1 = {
 
 
-str = {
   key1:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod magna aliqua. \nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
 
-  key2:
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod magna aliqua. \nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
-}
+    
 
-puts
-puts str[:key1]
+#   key2:
+#     "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod magna aliqua. \nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.",
+# }
+#
+# puts
+# puts str1[:key1]
+#
+#
+# str2 =  <<-EOS
+#           Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod magna aliqua. \nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
+#         EOS
+#
+# puts
+# puts str2
+#
+#
+# str3 = %(
+#         Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod magna aliqua. \nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.).lstrip
+#
+# puts
+# puts str3
 
+str4 = ['Lorem ipsum dolor sit amet, consectetur adipisicing',
+      'elit, sed do eiusmod tempor incididunt       ut labore et dolore',
+      'magna aliqua. \nUt enim ad minim veniam, quis nostrud',
+      'exercitation ullamco laboris nisi ut aliquip ex ea commodo',
+      'consequat. Duis aute irure dolor in reprehenderit in voluptate.']
+      .join
 
-str2 =  <<-EOS
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod magna aliqua. \nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.
-        EOS
-
-puts
-puts str2
-
-
-str3 = %(
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod magna aliqua. \nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate.).lstrip
-
-puts
-puts str3
+puts str4
